0d884eb122a463c302c9b572ab058a474462addc
HenriqueTGiuriati <henrique_giuriati@hotmail.com>
2016-06-22 20:53:18 -0300

    java project

---
+++ src/projetoSO/FileOrganizationModuleSimulator.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package projetoSO;
+
+/**
+ * CIAware :: Centro de Informatizações e Análises
+ * ----------------------------------------------- 
+ *
+ * @author Henrique Tresmonde Giuriati (Henrique, Jun 22, 2016)
+ * Responsabilidade da classe:
+ *
+ *
+ */
+
+/**
+ * CIAware :: Center of Informatization and Analysis
+ * -----------------------------------------------------
+ *
+ * @author Henrique Tresmonde Giuriati (Henrique, Jun 22, 2016) 
+ * Class responsibility: 
+ *
+ *
+ */
+
+public class FileOrganizationModuleSimulator {
+
+}
